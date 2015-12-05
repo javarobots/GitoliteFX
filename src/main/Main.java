227eb7dec84bf8adad020d2e4176b4a5dcfb9152
Jeramy Singleton <jeramyRR@gmail.com>
2015-12-05 15:41:13 -0600

    Organize Imports after merge

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,10 @@
 package main;
-	
+
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-import javafx.fxml.FXMLLoader;
-
+import javafx.stage.Stage;
 
 public class Main extends Application {
 	@Override
