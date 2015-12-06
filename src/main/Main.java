bdac4185e9a9830a24b59b18ab1397f5fee4428f
javarobots <jamey@theparhams.net>
2015-12-06 13:19:51 -0600

    Started working on view

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,17 +3,21 @@ package main;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("gitolite/gui/views/Main.fxml"));
-			Scene scene = new Scene(root,400,400);
+		    FXMLLoader loader = new FXMLLoader();
+		    loader.setLocation(getClass().getResource("gitolite/gui/views/Main.fxml"));
+			AnchorPane root = (AnchorPane)loader.load();
+			
+			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("Gitolite Repository Manager");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
--- src/main/gitolite/gui/controllers/MainViewController.java
+++ src/main/gitolite/gui/controllers/MainViewController.java
@@ -1,5 +1,25 @@
 package main.gitolite.gui.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.Tab;
+
 public class MainViewController {
+    
+    @FXML private MenuItem closeMenuItem;
+    @FXML private MenuItem aboutMenuItem; 
+    @FXML private Tab publicKeysTab;
+    
+    public void initialize()
+    {
+        setupCloseMenuItem();
+    }
+
+    private void setupCloseMenuItem()
+    {
+        closeMenuItem.setOnAction(event -> {
+            System.exit(0);
+        });
+    }
 	
 }
